3197e24297fe19ec5a6dee4ccf9d71b8ab084ef9
eguaman1990 <es.aguaman@gmail.com>
2015-03-25 06:17:05 -0300

    Merge origin/master

---
+++ Configuration/Configuracion.class.php
@@ -0,0 +1,19 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of Configuracion
+ *
+ * @author Edwin_Guaman
+ */
+class Configuracion {
+  static $HOST_DB='localhost';
+	static $NAME_BD='adminProyects';
+	static $USER_BD='root';
+	static $PASS_BD='';
+}
